a77c78162fcafc54936d8471cfc46d830bfa5d37
cod3monk <codemonk@u-sys.org>
2015-01-26 15:26:45 +0100

    added dummy and timing headers for automatic builds

---
+++ headers/timing.c
@@ -0,0 +1,17 @@
+#include "timing.h"
+
+void timing(double* wcTime, double* cpuTime)
+{
+   struct timeval tp;
+   struct rusage ruse;
+
+   gettimeofday(&tp, NULL);
+   *wcTime=(double) (tp.tv_sec + tp.tv_usec/1000000.0);
+
+   getrusage(RUSAGE_SELF, &ruse);
+   *cpuTime=(double)(ruse.ru_utime.tv_sec+ruse.ru_utime.tv_usec / 1000000.0);
+}
+
+void timing_(double* wcTime, double* cpuTime) {
+   timing(wcTime, cpuTime);
+}
---
+++ headers/timing.h
@@ -0,0 +1,9 @@
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/resource.h>
+
+
+
+void timing(double* wcTime, double* cpuTime);
