1abcb46fd56da750e210216c7316b39c0198cf26
cod3monk <codemonk@u-sys.org>
2015-01-29 18:52:11 +0100

    added more example kernels

---
+++ examples/3d-27pt.c
@@ -0,0 +1,22 @@
+double a[N][N][N];
+double b[N][N][N];
+double s;
+
+for(int k=1; k<N-1; ++k)
+    for(int j=1; j<N-1; ++j)
+        for(int i=1; i<N-1; ++i)
+            b[k][j][i] = ( a[k][j][i]
+                         + a[k][j][i-1] + a[k][j][i+1]
+                         + a[k][j-1][i] + a[k][j+1][i]
+                         + a[k-1][j][i] + a[k+1][j][i]
+                         + a[k][j-1][i-1] + a[k][j-1][i+1]
+                         + a[k][j+1][i-1] + a[k][j+1][i+1]
+                         + a[k-1][j][i-1] + a[k-1][j][i+1]
+                         + a[k+1][j][i-1] + a[k+1][j][i+1]
+                         + a[k-1][j-1][i] + a[k-1][j+1][i]
+                         + a[k+1][j-1][i] + a[k+1][j+1][i]
+                         + a[k-1][j-1][i-1] + a[k-1][j-1][i+1]
+                         + a[k-1][j+1][i-1] + a[k-1][j+1][i+1]
+                         + a[k+1][j-1][i-1] + a[k+1][j-1][i+1]
+                         + a[k-1][j+1][i-1] + a[k-1][j+1][i+1]
+                         ) * s;
