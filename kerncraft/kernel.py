03f1f7e11141a68d6d4821a76782ebe4274b423a
Julian Hammer <julian.hammer@fau.de>
2015-05-21 12:31:52 +0200

    added call to likwid_markerThreadInit() in benchmark code to support new 4.0 behaviour

--- kerncraft/kernel.py
+++ kerncraft/kernel.py
@@ -407,6 +407,8 @@ class Kernel:
         if type_ == 'likwid':
             # Call likwid_markerInit()
             ast.block_items.insert(0, c_ast.FuncCall(c_ast.ID('likwid_markerInit'), None))
+            # Call likwid_markerThreadInit()
+            ast.block_items.insert(1, c_ast.FuncCall(c_ast.ID('likwid_markerThreadInit'), None))
             # Call likwid_markerClose()
             ast.block_items.append(c_ast.FuncCall(c_ast.ID('likwid_markerClose'), None))
 
