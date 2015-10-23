2ee74780232e8575fdea187d651d6ff1080fe3cb
Julian Hammer <julian.hammer@fau.de>
2015-10-23 14:18:40 +0200

    added travis ci support

---
+++ tests/all_tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# as found in pycparser
+import sys
+sys.path[0:0] = ['.', '..']
+
+import unittest
+
+
+suite = unittest.TestLoader().loadTestsFromNames(
+    [
+        'test_kerncraft',
+    ]
+)
+
+testresult = unittest.TextTestRunner(verbosity=1).run(suite)
+sys.exit(0 if testresult.wasSuccessful() else 1)
