cf7d626e73cead22754b52342d120ba2fbe33bc6
Julian Hammer <julian.hammer@fau.de>
2015-10-28 15:50:19 +0100

    added kerncraft.h to package_data

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'kerncraft': ['headers/dummy.c', 'README', 'LICENSE'],
+        'kerncraft': ['headers/dummy.c', 'headers/kerncraft.h', 'README', 'LICENSE'],
         'examples': [
             'machine-files/*.yaml',
             'kernels/*.c',
