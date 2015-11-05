fb11a9d8b853e480442b897d4f74d08fc943bb8b
Julian Hammer <julian.hammer@fau.de>
2015-11-05 16:37:34 +0100

    added forgotten dummy_likwid files

---
+++ tests/test_files/dummy_likwid/include/likwid.h
@@ -0,0 +1,5 @@
+void likwid_markerInit(void) {};
+void likwid_markerThreadInit(void) {};
+int likwid_markerStartRegion(const char* regionTag) {return 0;};
+int likwid_markerStopRegion(const char* regionTag) {return 0;};
+void likwid_markerClose(void) {};
