57d26ecb56e2269152b7ca7e00f8657031f3d377
Julian Hammer <julian.hammer@fau.de>
2016-07-05 23:10:53 +0200

    more verbose outputs with help for manual editing

--- kerncraft/likwid_bench_auto.py
+++ kerncraft/likwid_bench_auto.py
@@ -4,14 +4,15 @@ from __future__ import absolute_import
 
 # Version check
 import sys
-if sys.version_info[0] == 2 and sys.version_info < (2, 7) or \
+if sys.version_info[0] == 2 and sys.version_info < (2, 6) or \
         sys.version_info[0] == 3 and sys.version_info < (3, 4):
-    print("Must use python 2.7 or 3.4 and greater.", file=sys.stderr)
+    print("Must use python 2.6 or 3.4 and greater.", file=sys.stderr)
     sys.exit(1)
 
 import subprocess
 import re
 from copy import copy
+from pprint import pprint
 
 from ruamel import yaml
 
@@ -21,12 +22,17 @@ from six.moves import range
 
 def get_match_or_break(regex, haystack, flags=re.MULTILINE):
     m = re.search(regex, haystack, flags)
-    assert m, "could not find "+repr(regex)
+    if not m:
+        raise ValueError("could not find "+repr(regex)+" in "+repr(haystack))
     return m.groups()
 
 
 def get_machine_topology():
-    topo = subprocess.Popen(['likwid-topology'], stdout=subprocess.PIPE).communicate()[0]
+    try:
+        topo = subprocess.Popen(['likwid-topology'], stdout=subprocess.PIPE).communicate()[0]
+    except OSError as e:
+        print('likwid-topology execution failed, is it installed and loaded?', file=sys.stderr)
+        sys.exit(1)
     cpuinfo = open('/proc/cpuinfo', 'r').read()
     machine = {
         'model type': get_match_or_break(r'^CPU type:\s+(.+?)\s*$', topo)[0],
@@ -34,14 +40,22 @@ def get_machine_topology():
         'sockets': int(get_match_or_break(r'^Sockets:\s+([0-9]+)\s*$', topo)[0]),
         'cores per socket': int(get_match_or_break(r'^Cores per socket:\s+([0-9]+)\s*$', topo)[0]),
         'threads per core': int(get_match_or_break(r'^Threads per core:\s+([0-9]+)\s*$', topo)[0]),
-        'clock': 'INFORMATION_REQUIRED',
-        'FLOPs per cycle': 'INFORMATION_REQUIRED',
-        'micro-architecture': 'INFORMATION_REQUIRED',
-        'compiler': 'INFORMATION_REQUIRED',
-        'compiler flags': 'INFORMATION_REQUIRED',
-        'cacheline size': 'INFORMATION_REQUIRED',
-        'overlapping ports': 'INFORAMTION_REQUIRED',
-        'non-overlapping ports': 'INFORMATION_REQUIRED',
+        'clock': 'INFORMATION_REQUIRED (e.g., 2.7 GHz)',
+        'FLOPs per cycle': {'SP': {'total': 'INFORMATION_REQUIRED',
+                                   'FMA': 'INFORMATION_REQUIRED',
+                                   'ADD': 'INFORMATION_REQUIRED',
+                                   'MUL': 'INFORMATION_REQUIRED'},
+                            'DP': {'total': 'INFORMATION_REQUIRED',
+                                   'FMA': 'INFORMATION_REQUIRED',
+                                   'ADD': 'INFORMATION_REQUIRED',
+                                   'MUL': 'INFORMATION_REQUIRED'}},
+        'micro-architecture': 'INFORMATION_REQUIRED (options: NHM, WSM, SNB, IVB, HSW)',
+        'compiler': 'INFORMATION_REQUIRED (e.g., gcc)',
+        'compiler flags': 'INFORMATION_REQUIRED (list of flags, e.g., [-O3, -xACX, -fno-alias])',
+        'cacheline size': 'INFORMATION_REQUIRED (in bytes, e.g. 64 B)',
+        'overlapping ports': 'INFORAMTION_REQUIRED (list of ports as they appear in IACA, e.g.)' + \
+                             ', ["0", "0DV", "1", "2", "3", "4", "5", "6", "7"])',
+        'non-overlapping ports': 'INFORMATION_REQUIRED (like overlapping ports)',
     }
 
     threads_start = topo.find('HWThread')
@@ -58,10 +72,21 @@ def get_machine_topology():
     mem_level = {}
     for line in topo[cache_start:cache_end].split('\n'):
         if line.startswith('Level:'):
+            mem_level = {}
             mem_level['level'] = 'L'+line.split(':')[1].strip()
+            machine['memory hierarchy'].append(mem_level)
         elif line.startswith('Size:'):
-            mem_level['cache per group'] = 
-                'INFORMATION_REQUIRED ('+str(PrefixedUnit(line.split(':')[1].strip()))+')'
+            size = PrefixedUnit(line.split(':')[1].strip())
+            mem_level['cache per group'] = {
+                'sets': 'INFORMATION_REQUIRED (sets*ways*cl_size='+str(size)+')',
+                'ways': 'INFORMATION_REQUIRED (sets*ways*cl_size='+str(size)+')',
+                'cl_size': 'INFORMATION_REQUIRED (sets*ways*cl_size='+str(size)+')',
+                'replacement_policy': 'INFORMATION_REQUIRED (options: LRU, FIFO, MRU, RR)',
+                'write_allocate': 'INFORMATION_REQUIRED (True/False)',
+                'write_back': 'INFORMATION_REQUIRED (True/False)',
+                'load_from': 'L'+str(int(mem_level['level'][1:])+1),
+                'store_to': 'L'+str(int(mem_level['level'][1:])+1)}
+            mem_level['size per group'] = size
         elif line.startswith('Cache groups:'):
             mem_level['groups'] = line.count('(')
             mem_level['cores per group'] = \
@@ -70,15 +95,17 @@ def get_machine_topology():
                 mem_level['cores per group'] * machine['threads per core']
         mem_level['cycles per cacheline transfer'] = 'INFORMATION_REQUIRED'
 
-        if len(mem_level) == 7:
-            machine['memory hierarchy'].append(mem_level)
-            mem_level = {}
+    # Remove last caches load_from and store_to:
+    del machine['memory hierarchy'][-1]['cache per group']['load_from']
+    del machine['memory hierarchy'][-1]['cache per group']['store_to']
+    
     machine['memory hierarchy'].append({
         'level': 'MEM',
         'cores per group': machine['cores per socket'],
         'threads per group': machine['threads per core'] * machine['cores per socket'],
         'cycles per cacheline transfer': None,
-        'penalty cycles per read stream': 0
+        'penalty cycles per read stream': 0,
+        'size per group': None
     })
 
     return machine
@@ -97,6 +124,10 @@ def measure_bw(type_, total_size, threads_per_core, max_threads_per_core, cores_
     # for older likwid versions add ['-g', str(sockets), '-i', str(iterations)] to cmd
     cmd = ['likwid-bench', '-t', type_]+groups
     output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    if not output:
+        print(' '.join(cmd) + ' returned no output, possibly wrong version installed '
+              '(requires 4.0 or later)', file=sys.stderr)
+        sys.exit(1)
     bw = float(get_match_or_break(r'^MByte/s:\s+([0-9]+(?:\.[0-9]+)?)\s*$', output)[0])
     return PrefixedUnit(bw, 'MB/s')
 
@@ -121,6 +152,7 @@ def cli():
 
 def main():
     machine = get_machine_topology()
+    pprint(machine)
 
     benchmarks = {'kernels': {}, 'measurements': {}}
     machine['benchmarks'] = benchmarks
@@ -162,12 +194,12 @@ def main():
             threads = [c*threads_per_core for c in cores]
             if mem['size per group'] is not None:
                 total_sizes = [
-                    max(mem['size per group']*c/mem['cores per group'],
-                        mem['size per group'])*USAGE_FACTOR
+                    max(int(mem['size per group'])*c/mem['cores per group'],
+                        int(mem['size per group']))*USAGE_FACTOR
                     for c in cores]
             else:
                 last_mem = machine['memory hierarchy'][-2]
-                total_sizes = [last_mem['size per group']/USAGE_FACTOR for c in cores]
+                total_sizes = [float(last_mem['size per group'])/USAGE_FACTOR for c in cores]
             sizes_per_core = [t/cores[i] for i, t in enumerate(total_sizes)]
             sizes_per_thread = [t/threads[i] for i, t in enumerate(total_sizes)]
 
