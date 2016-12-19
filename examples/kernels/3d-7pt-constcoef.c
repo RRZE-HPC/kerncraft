737e3581171c274773ac14faedfde7d1ad89079a
Julian Hammer <julian.hammer@fau.de>
2016-12-19 10:29:53 +0100

    fixed const coeff 3d-7pt kernel

--- examples/kernels/3d-7pt-constcoef.c
+++ examples/kernels/3d-7pt-constcoef.c
@@ -1,13 +1,14 @@
 double a[M][N][N];
 double b[M][N][N];
-double coeffs[6], s;
+double coeffs_N, coeffs_S, coeffs_W, coeffs_E,
+       coeffs_F, coeffs_B, s;
 
 for(int k=1; k<M-1; ++k)
     for(int j=1; j<N-1; ++j)
         for(int i=1; i<N-1; ++i)
-            b[k][j][i] = ( coeffs[0]*a[k][j][i-1]
-                         + coeffs[1]*a[k][j][i+1]
-                         + coeffs[2]*a[k][j-1][i]
-                         + coeffs[3]*a[k][j+1][i]
-                         + coeffs[4]*a[k-1][j][i]
-                         + coeffs[5]*a[k+1][j][i]) * s;
+            b[k][j][i] = ( coeffs_W*a[k][j][i-1]
+                         + coeffs_E*a[k][j][i+1]
+                         + coeffs_N*a[k][j-1][i]
+                         + coeffs_S*a[k][j+1][i]
+                         + coeffs_B*a[k-1][j][i]
+                         + coeffs_F*a[k+1][j][i]) * s;
