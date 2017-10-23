0a275311bd8e17ae868d3fb70532afd7b4082f27
Julian Hammer <julian.hammer@fau.de>
2017-10-23 13:10:53 +0200

    added useful layer condition test

--- tests/test_files/3d-7pt.c
+++ tests/test_files/3d-7pt.c
@@ -3,7 +3,7 @@ double b[M][N][N];
 double s;
 
 for(int k=1; k<M-1; ++k)
-    for(int j=1; j<N-1; j+=2)
+    for(int j=1; j<N-1; j+=1)
         for(int i=1; i<N-1; ++i)
             b[k][j][i] = ( a[k][j][i]
                          + a[k][j][i-1] + a[k][j][i+1]
--- tests/test_layer_condition.py
+++ tests/test_layer_condition.py
@@ -25,6 +25,13 @@ from kerncraft import kerncraft as kc
 from kerncraft.prefixedunit import PrefixedUnit
 
 
+def recursive_dict_get(d, key_path):
+    if len(key_path) > 1:
+        return recursive_dict_get(d[key_path[0]], key_path[1:])
+    else:
+        return d[key_path[0]]
+
+
 class TestLayerCondition(unittest.TestCase):
     def setUp(self):
         # Create a temporary directory
@@ -50,6 +57,67 @@ class TestLayerCondition(unittest.TestCase):
         with self.assertRaises(ValueError) as cm:
             kc.run(parser, args, output_file=output_stream)
 
+    def test_3d_7pt(self):
+        store_file = os.path.join(self.temp_dir, 'test_3d7pt_LC.pickle')
+        output_stream = StringIO()
+        parser = kc.create_parser()
+        args = parser.parse_args(['-m', self._find_file('phinally_gcc.yaml'),
+                                  '-p', 'LC',
+                                  self._find_file('3d-7pt.c'),
+                                  '-D', 'N', '0',
+                                  '-D', 'M', '0',
+                                  '-vvv',
+                                  '--store', store_file])
+        kc.check_arguments(args, parser)
+        kc.run(parser, args, output_file=output_stream)
+        results = pickle.load(open(store_file, 'rb'))
+        result = next(iter(results['3d-7pt.c'].values()))['LC']
+
+        N = sympy.var('N')
+        result_expected = {
+            'dimensions': {
+                1: {
+                    'slices_sum': 2,
+                    'slices_count': 6,
+                    'caches': {
+                        'L1': {'lt': True},
+                        'L2': {'lt': True},
+                        'L3': {'lt': True},
+                    },
+                    'cache_requirement_bytes': 64,
+                },
+                2: {
+                    'slices_sum': 2*N,
+                    'slices_count': 4,
+                    'caches': {
+                        'L1': {'lt': 48*N - 32 <= 32768},
+                        'L2': {'lt': 48*N - 32 <= 262144},
+                        'L3': {'lt': 48*N - 32 <= 20971520},
+                    },
+                    'cache_requirement_bytes': 48*N - 32,
+                },
+                3: {
+                    'slices_sum': 2*N*(N-1)+2*N,
+                    'slices_count': 2,
+                    'caches': {
+                        'L1': {'lt': 16*N**2 + 16*N*(N - 1) <= 32768},
+                        'L2': {'lt': 16*N**2 + 16*N*(N - 1) <= 262144},
+                        'L3': {'lt': 16*N**2 + 16*N*(N - 1) <= 20971520},
+                    },
+                    'cache_requirement_bytes': 16*N**2 + 16*N*(N - 1),
+                },
+            }
+        }
+
+        # Iterate over expected results and validate with generated results
+        stack = [((k,), v) for k, v in result_expected.items()]
+        while stack:
+            key_path, value = stack.pop()
+            if isinstance(value, dict):
+                stack.extend([(key_path + (k,), v) for k, v in value.items()])
+            else:
+                self.assertEqual(value, recursive_dict_get(result, key_path),
+                                 msg="at key_path={}".format(key_path))
 
 if __name__ == '__main__':
     unittest.main()
