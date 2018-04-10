59a12555742740ac4b0e111d5f928d9ea941ad7c
Julian Hammer <julian.hammer@fau.de>
2018-04-10 13:17:46 +0200

    fixed skylake L1-L2 to 64 B/cy

--- kerncraft/__init__.py
+++ kerncraft/__init__.py
@@ -1,6 +1,12 @@
 """Kerncraft static analytical performance modeling framework and tool."""
 __version__ = '0.6.7'
 
+# To trigger travis deployment to pypi, do the following:
+# 1. Increment __version___
+# 2. commit to RRZE-HPC/kerncraft's master branch
+# 3. wait for travis to complete successful (unless already tested)
+# 4. tag commit with 'v{}'.format(__version__) (`git tag vX.Y.Z`)
+# 5. push tag to github (`git push origin vX.Y.Z`)
 
 def get_header_path():
     """Return local folder path of header files."""
