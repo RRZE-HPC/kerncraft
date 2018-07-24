897928f3ac1c08e402f4a1342e624c350cd5bfde
Julian Hammer <julian.hammer@fau.de>
2018-07-24 13:23:50 +0200

    updated travis deploy password

--- kerncraft/__init__.py
+++ kerncraft/__init__.py
@@ -6,7 +6,7 @@ __version__ = '0.6.9'
 # 2. commit to RRZE-HPC/kerncraft's master branch
 # 3. wait for travis to complete successful (unless already tested)
 # 4. tag commit with 'v{}'.format(__version__) (`git tag vX.Y.Z`)
-# 5. push tag to github (`git push origin vX.Y.Z`)
+# 5. push tag to github (`git push origin vX.Y.Z` or push all tags with `git push --tags`)
 
 def get_header_path():
     """Return local folder path of header files."""
