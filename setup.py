db44b028824990dce684f007288fe918da4df912
Julian Hammer <julian.hammer@fau.de>
2018-09-18 14:35:48 +0200

    added py37

--- setup.py
+++ setup.py
@@ -76,6 +76,9 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
