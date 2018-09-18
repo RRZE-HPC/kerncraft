034ebe5c6fd989fe95f85f46920a3ce443ac1a88
Julian Hammer <julian.hammer@fau.de>
2018-09-18 14:48:17 +0200

    adde missed test file

---
+++ tests/test_machinemodel.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Tests for validity of example files.
+"""
+import os
+from glob import glob
+import unittest
+
+from kerncraft import machinemodel
+from kerncraft.prefixedunit import PrefixedUnit
+
+
+class TestMachineModel(unittest.TestCase):
+    @staticmethod
+    def _find_file(name):
+        testdir = os.path.dirname(__file__)
+        name = os.path.join(testdir, 'test_files', name)
+        assert os.path.exists(name)
+        return name
+
+    def setUp(self):
+        self.machine = machinemodel.MachineModel(self._find_file('phinally_gcc.yaml'))
+
+    def test_types(self):
+        self.assertEqual(type(self.machine['clock']), PrefixedUnit)
+        self.assertEqual(self.machine['clock'].base_value(), 2700000000.0)
+        self.assertEqual(self.machine['clock'].unit, "Hz")
