20fbf3d6795ffd41edda2ac2c673523f1b52ee8c
Julian Hammer <julian.hammer@fau.de>
2018-09-24 15:37:39 +0200

    version bump

--- kerncraft/__init__.py
+++ kerncraft/__init__.py
@@ -1,5 +1,5 @@
 """Kerncraft static analytical performance modeling framework and tool."""
-__version__ = '0.6.10'
+__version__ = '0.7.0'
 
 # To trigger travis deployment to pypi, do the following:
 # 1. Increment __version___
