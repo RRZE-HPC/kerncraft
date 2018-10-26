507fa956a34cb7640488717934a1fa8a4d76febd
Julian Hammer <julian.hammer@fau.de>
2018-10-26 09:39:57 +0200

    version bump

--- kerncraft/__init__.py
+++ kerncraft/__init__.py
@@ -1,5 +1,5 @@
 """Kerncraft static analytical performance modeling framework and tool."""
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 # To trigger travis deployment to pypi, do the following:
 # 1. Increment __version___
@@ -8,7 +8,8 @@ __version__ = '0.7.1'
 # 4. tag commit with 'v{}'.format(__version__) (`git tag vX.Y.Z`)
 # 5. push tag to github (`git push origin vX.Y.Z` or push all tags with `git push --tags`)
 
-def get_header_path():
+
+def get_header_path() -> str:
     """Return local folder path of header files."""
     import os
     return os.path.abspath(os.path.dirname(os.path.realpath(__file__))) + '/headers/'
