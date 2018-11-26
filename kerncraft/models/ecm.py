799b6c32dc532ec09ae5dbdcbdbc4431310a6860
Julian Hammer <julian.hammer@fau.de>
2018-11-26 14:30:37 +0100

    fixed out-of-socket scaling calculation

--- kerncraft/models/ecm.py
+++ kerncraft/models/ecm.py
@@ -477,29 +477,27 @@ class ECM(PerformanceModel):
             report += "\nprediction for {} cores,".format(self._args.cores) + \
                       " assuming static scheduling:\n"
 
-            # in-socket prediction:
+            # out-of-core scaling prediction:
             cores_per_numa_domain = self.machine['cores per NUMA domain']
             innuma_cores = min(self._args.cores, cores_per_numa_domain)
             if innuma_cores <= self.results['scaling cores']:
-                insocket = PrefixedUnit(
+                innuma_rectp = PrefixedUnit(
                     max(sum([c[1] for c in self.results['cycles']]) + self.results['T_nOL'],
                         self.results['T_OL'])/innuma_cores,
                     "cy/CL")
                 note = "memory-interface not saturated"
             else:
-                insocket = PrefixedUnit(self.results['cycles'][-1][1], 'cy/CL')
+                innuma_rectp = PrefixedUnit(self.results['cycles'][-1][1], 'cy/CL')
                 note = "memory-interface saturated on first socket"
 
             if 0 < self._args.cores <= cores_per_numa_domain:
                 # only in-numa scaling to consider
-                report += "{}".format(self._CPU.conv_cy(insocket, self._args.unit))
+                report += "{}".format(self._CPU.conv_cy(innuma_rectp, self._args.unit))
                 note += ", in-NUMA-domain scaling"
             elif self._args.cores <= self.machine['cores per socket']*self.machine['sockets']:
                 # out-of-numa scaling behavior
-                report += "{}".format(self._CPU.conv_cy(
-                    insocket * (1 - (self._args.cores - cores_per_numa_domain)/
-                                (math.ceil(self._args.cores / cores_per_numa_domain) *
-                                 cores_per_numa_domain)),
+                report += "{}".format(
+                    self._CPU.conv_cy(innuma_rectp * innuma_cores / self._args.cores,
                     self._args.unit))
                 note += ", out-of-NUMA-domain scaling"
             else:
