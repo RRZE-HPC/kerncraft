0362ba464992d9c3c477827e4b93b9357cc6060e
Julian Hammer <codemonk@u-sys.org>
2022-02-17 13:32:59 +0100

    moving forward in time. Python <3.7 support droped, added 3.9 and 3.10

--- setup.py
+++ setup.py
@@ -76,11 +76,10 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     # What does your project relate to?
@@ -108,7 +107,7 @@ setup(
         'psutil',
         'compress_pickle',
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.7',
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
